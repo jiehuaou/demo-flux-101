eb51a0cad0fb4dd46f6c112ae46a642c2c645d4b
albert ou <albert_ou@epam.com>
2021-10-21 11:23:48 +0800

    flux : create flux Programmatically

--- src/test/java/com/example/flux/FluxApplicationTests.java
+++ src/test/java/com/example/flux/FluxApplicationTests.java
@@ -87,13 +87,18 @@ class FluxApplicationTests {
 				.verify();
 	}
 
+	/**
+	 * Programmatically create a Flux by generating signals one-by-one via a consumer callback and some state.
+	 */
 	@Test
 	void testGenerateFlux(){
 		Flux<String> flux = Flux.generate(
 				() -> 0,
 				(state, sink) -> {
 					sink.next("3 x " + state + " = " + 3*state);
-					if (state == 10) sink.complete();
+					if (state == 10) {
+						sink.complete();
+					}
 					return state + 1;
 				});
 		flux.log().blockLast();
