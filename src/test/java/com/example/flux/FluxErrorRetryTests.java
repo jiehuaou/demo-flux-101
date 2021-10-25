a99b778e4d64504ffc4bd6596a1246dd6cbce845
albert ou <albert_ou@epam.com>
2021-10-25 11:19:38 +0800

    flux-101 : error retry with resource

--- src/test/java/com/example/flux/FluxErrorRetryTests.java
+++ src/test/java/com/example/flux/FluxErrorRetryTests.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.ByteArrayInputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+
 @Log4j2
 public class FluxErrorRetryTests {
 
@@ -69,6 +74,81 @@ public class FluxErrorRetryTests {
         //.subscribe(e -> log.info("sub -> {}", e));
 
         log.info("test end");
+    }
+
+    static public void closeStream(Closeable s){
+        try{
+            log.info("close io-stream {}", s);
+            if(s!=null)s.close();
+        }catch(IOException e){
+            log.error(e);
+        }
+    }
+
+    static int processStream(InputStream ins) {
+        byte[] buf = new byte[3];
+        int total = 0;
+
+        log.info("process io-stream {}", ins);
+
+        try {
+            while (ins.available()>0){
+                final int len = ins.read(buf);
+                total += len;
+                log.info("read ----> {}", total);
+                if(total>=10){
+                    throw new RuntimeException("fake error");
+                }
+            }
+        } catch (IOException e) {
+            log.error(e);
+        } finally {
+            closeStream(ins);
+        }
+        
+        return total;
+    }
+    
+    public static InputStream openStream(String data) {
+        log.info("open io-stream {}", data);
+        return  new ByteArrayInputStream(data.getBytes());
+    }
+
+    /**
+     * test retry with error happening
+     * when string length >= 10, will trigger runtime exception
+     */
+    @Test
+    void testMonoErrorRetry(){
+        Mono.just("hello world")
+                .map(x-> openStream(x))
+                .map(x->processStream(x))
+                .retry(2)
+                .doOnError(e->log.info("something wrong with {}", e.toString()))
+                .doOnSuccess(e->log.info("success with {}", e))
+                .onErrorResume((e)->Mono.just(0))  // to disable the last exception
+                .doFinally(e->{
+                    log.info("done with {}", e);
+                })
+                .block();
+    }
+
+    /**
+     * test retry without error happening
+     */
+    @Test
+    void testMonoNoRetry(){
+        Mono.just("hello")
+                .map(x-> openStream(x))
+                .map(x->processStream(x))
+                .retry(2)
+                .doOnError(e->log.info("something wrong with {}", e.toString()))
+                .doOnSuccess(e->log.info("success with {}", e))
+                .onErrorResume((e)->Mono.just(0))
+                .doFinally(e->{
+                    log.info("done with {}", e);
+                })
+                .block();
     }
 
 }
